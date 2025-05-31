5727c7bdf23bc26e30ac8c283de88acb8a7a74c1
buhgit3124 <buhgit3124@gmail.com>
2025-05-31 13:18:43 -0700

    dsdsf

--- Tools/AddCopyright/GlobalSuppressions.cs
+++
@@ -1,3 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Roslynator", "RCS1197:Optimize StringBuilder.Append/AppendLine call.", Justification = "<Pending>", Scope = "member", Target = "~M:AddCopyright.Program.Main(System.String[])")]
